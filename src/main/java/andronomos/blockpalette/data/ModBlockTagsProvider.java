c155da64b011d03fc9b6e47e2ab9321a2f474b4b
Andronomos <andronomos@proton.me>
2023-12-22 23:43:15 -0800

    Remove unnecessary imports

--- src/main/java/andronomos/blockpalette/data/ModBlockTagsProvider.java
+++ src/main/java/andronomos/blockpalette/data/ModBlockTagsProvider.java
@@ -5,10 +5,8 @@ import andronomos.blockpalette.registry.ModBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.world.level.block.*;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.Nullable;
 
@@ -29,7 +27,7 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
 		ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(b -> {
 			String blockType = b.getClass().getSimpleName();
 
-			switch(blockType) {
+			switch (blockType) {
 				case "ConcretePowderBlock" -> tag(BlockTags.MINEABLE_WITH_SHOVEL).add(b);
 				default -> tag(BlockTags.MINEABLE_WITH_PICKAXE).add(b);
 			}
