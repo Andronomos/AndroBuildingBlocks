f7c3c7b9f3c8c245ff6271787a55d29275617732
Andronomos <andronomos@proton.me>
2023-12-22 23:43:15 -0800

    Fixed rough concrete powder block tags

--- src/main/java/andronomos/blockpalette/data/ModBlockTagsProvider.java
+++ src/main/java/andronomos/blockpalette/data/ModBlockTagsProvider.java
@@ -5,11 +5,10 @@ import andronomos.blockpalette.registry.ModBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.world.level.block.FenceBlock;
-import net.minecraft.world.level.block.StainedGlassBlock;
-import net.minecraft.world.level.block.WallBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.Nullable;
 
@@ -28,10 +27,15 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
 	@Override
 	protected void addTags(HolderLookup.Provider provider) {
 		ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(b -> {
-			tag(BlockTags.MINEABLE_WITH_PICKAXE).add(b);
+			String blockType = b.getClass().getSimpleName();
 
-			if(b instanceof WallBlock wall) {
-				tag(BlockTags.WALLS).add(wall);
+			switch(blockType) {
+				case "ConcretePowderBlock" -> tag(BlockTags.MINEABLE_WITH_SHOVEL).add(b);
+				default -> tag(BlockTags.MINEABLE_WITH_PICKAXE).add(b);
+			}
+
+			if(blockType.equals("WallBlock")) {
+				tag(BlockTags.WALLS).add(b);
 			}
 		});
 	}
