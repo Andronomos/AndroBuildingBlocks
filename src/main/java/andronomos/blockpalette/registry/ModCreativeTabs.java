978ed1b834ccdfca8ebbf2a25d9b4b323e675cb9
Andronomos <andronomos@proton.me>
2023-12-22 23:43:13 -0800

    .

--- src/main/java/andronomos/blockpalette/registry/ModCreativeTabs.java
+++ src/main/java/andronomos/blockpalette/registry/ModCreativeTabs.java
@@ -4,7 +4,6 @@ import andronomos.blockpalette.BlockPalette;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
