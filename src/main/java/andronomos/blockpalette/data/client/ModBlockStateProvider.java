91fa8e22c4fa41f7476e46f4d0a5c300a38c4be0
Andronomos <andronomos@proton.me>
2023-12-24 15:32:47 -0800

    Removed the texture folder logic from datagen

--- src/main/java/andronomos/blockpalette/data/client/ModBlockStateProvider.java
+++ src/main/java/andronomos/blockpalette/data/client/ModBlockStateProvider.java
@@ -3,7 +3,6 @@ package andronomos.blockpalette.data.client;
 import andronomos.blockpalette.BlockPalette;
 import andronomos.blockpalette.registry.ModBlocks;
 import net.minecraft.core.Direction;
-import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.*;
@@ -25,107 +24,62 @@ public class ModBlockStateProvider extends BlockStateProvider {
 			String blockName = ForgeRegistries.BLOCKS.getKey(b).getPath();
 			String blockType = b.getClass().getSimpleName();
 			//String textureFolder = blockName;
-			String textureFolder = "";
-
-			if(blockName.contains("rough") && blockName.contains("concrete")) {
-				textureFolder = "rough_concrete";
-			}
-
-			if(blockName.contains("barricade")) {
-				textureFolder = "barricade";
-			}
-
-			if(blockName.contains("borderless")) {
-				textureFolder = "borderless_glass";
-			}
 
 			switch (blockType) {
-				case "StairBlock" -> registerStairBlockStateAndModel((StairBlock)b, blockName.substring(0, blockName.indexOf("_stairs")), textureFolder);
-				case "SlabBlock" -> registerSlabBlockStateAndModel((SlabBlock)b, blockName, textureFolder);
-				case "WallBlock" -> registerWallBlockStateAndModel((WallBlock)b, blockName.substring(0, blockName.indexOf("_wall")), textureFolder);
-				case "StainedGlassBlock" -> registerGlassBlockStateAndModel(b, blockName, textureFolder);
-				case "StainedGlassPaneBlock" -> registerPaneBlockStateAndModel((IronBarsBlock)b, blockName, textureFolder);
-				case "RotatedPillarBlock" -> registerRotatableBlockStateAndModel((RotatedPillarBlock) b, blockName, textureFolder,  "");
-				default -> registerBlockStateAndModel(b, blockName, textureFolder);
+				case "StairBlock" -> registerStairBlockStateAndModel((StairBlock)b, blockName.substring(0, blockName.indexOf("_stairs")));
+				case "SlabBlock" -> registerSlabBlockStateAndModel((SlabBlock)b, blockName);
+				case "WallBlock" -> registerWallBlockStateAndModel((WallBlock)b, blockName.substring(0, blockName.indexOf("_wall")));
+				case "StainedGlassBlock" -> registerGlassBlockStateAndModel(b, blockName);
+				case "StainedGlassPaneBlock" -> registerPaneBlockStateAndModel((IronBarsBlock)b, blockName);
+				case "RotatedPillarBlock" -> registerRotatableBlockStateAndModel((RotatedPillarBlock) b, blockName, "");
+				default -> registerBlockStateAndModel(b, blockName);
 			}
 		});
 	}
 
-	private void registerBlockStateAndModel(Block block, String name, String textureFolder) {
-		ModelFile model = !textureFolder.equals("") ? models().cubeAll(name, modLoc("block/" + textureFolder + "/" + name))
-				: models().cubeAll(name, modLoc("block/" + name));
+	private void registerBlockStateAndModel(Block block, String name) {
+		ModelFile model = models().cubeAll(name, modLoc("block/" + name));
 		simpleBlock(block, model);
 		registerItemModel(name);
 	}
 
-	private void registerStairBlockStateAndModel(StairBlock block, String name, String textureFolder) {
-		if(!textureFolder.equals("")) {
-			stairsBlock(block, modLoc("block/" + textureFolder + "/" + name));
-		} else {
-			stairsBlock(block, modLoc("block/" + name));
-		}
+	private void registerStairBlockStateAndModel(StairBlock block, String name) {
+		stairsBlock(block, modLoc("block/" + name));
 		registerItemModel(name + "_stairs");
 	}
 
-	private void registerSlabBlockStateAndModel(SlabBlock block, String name, String textureFolder) {
+	private void registerSlabBlockStateAndModel(SlabBlock block, String name) {
 		String cleanName = name.substring(0, name.indexOf("_slab"));
-
-		if(!textureFolder.equals("")) {
-			slabBlock(block, modLoc("block/" + cleanName), modLoc("block/" + textureFolder + "/" + cleanName));
-		} else {
-			slabBlock(block, modLoc("block/" + cleanName), modLoc("block/" + cleanName));
-		}
+		slabBlock(block, modLoc("block/" + cleanName), modLoc("block/" + cleanName));
 		registerItemModel(name);
 	}
 
-	private void registerWallBlockStateAndModel(WallBlock block, String name, String textureFolder) {
-		if(!textureFolder.equals("")) {
-			wallBlock(block, modLoc("block/" + textureFolder + "/" + name));
-			itemModels().wallInventory(name + "_wall", modLoc("block/" + textureFolder + "/" + name));
-		} else {
-			wallBlock(block, modLoc("block/" + name));
-			itemModels().wallInventory(name + "_wall", modLoc("block/" + name));
-		}
+	private void registerWallBlockStateAndModel(WallBlock block, String name) {
+		wallBlock(block, modLoc("block/" + name));
+		itemModels().wallInventory(name + "_wall", modLoc("block/" + name));
 	}
 
-	private void registerGlassBlockStateAndModel(Block block, String name, String textureFolder) {
+	private void registerGlassBlockStateAndModel(Block block, String name) {
 		ModelFile model;
-
-		if (!textureFolder.equals("")) {
-			model = models().cubeAll(name, modLoc("block/" + textureFolder + "/" + name))
-					.renderType("translucent");
-		} else {
-			model = models().cubeAll(name, modLoc("block/" + name))
-					.renderType("translucent");
-		}
-
+		model = models().cubeAll(name, modLoc("block/" + name)).renderType("translucent");
 		simpleBlock(block, model);
 		registerItemModel(name);
 	}
 
-	private void registerPaneBlockStateAndModel(IronBarsBlock block, String blockName, String textureFolder) {
+	private void registerPaneBlockStateAndModel(IronBarsBlock block, String blockName) {
 		String cleanName = blockName.replace("_pane", "");
-		String textureName = String.format("block/%s/%s", textureFolder, cleanName);
+		String textureName = String.format("block/%s", cleanName);
 		paneBlockWithRenderType(block, modLoc(textureName), modLoc(textureName),"translucent");
 		itemModels().singleTexture(blockName, mcLoc("item/generated"), "layer0", modLoc(textureName)).renderType("translucent");
 	}
 
-	private void registerRotatableBlockStateAndModel(RotatedPillarBlock block, String name, String textureFolder, String topTexture) {
-		String resource = "block/" + textureFolder + "/" + name;
-
-		if(textureFolder.equals("")) {
-			resource = "block/" + name;
-		}
-
+	private void registerRotatableBlockStateAndModel(RotatedPillarBlock block, String name, String topTexture) {
+		String resource = "block/" + name;
 		ResourceLocation sides = modLoc(resource);
 		ResourceLocation end;
 
 		if(!topTexture.equals("")) {
-			if(!textureFolder.equals("")) {
-				end = modLoc("block/" + textureFolder + "/" + topTexture);
-			} else {
-				end = modLoc("block/" + topTexture);
-			}
+			end = modLoc("block/" + topTexture);
 		} else {
 			end = sides;
 		}
