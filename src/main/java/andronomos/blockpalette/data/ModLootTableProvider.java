e56d01a09c1c6d1fa32edc3f4ab04c7204dc43dc
Andronomos <andronomos@proton.me>
2024-02-05 08:52:06 -0800

    Removed some whitespace

--- src/main/java/andronomos/blockpalette/data/ModLootTableProvider.java
+++ src/main/java/andronomos/blockpalette/data/ModLootTableProvider.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class ModLootTableProvider {
 	public static LootTableProvider create(PackOutput output) {
 		return new LootTableProvider(output, Set.of(), List.of(
-				new LootTableProvider.SubProviderEntry(ModBlockLootSubProvider::new, LootContextParamSets.BLOCK)
+			new LootTableProvider.SubProviderEntry(ModBlockLootSubProvider::new, LootContextParamSets.BLOCK)
 		));
 	}
 }
