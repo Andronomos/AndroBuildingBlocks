a98e9f9257c77e6ac1b6a7f4b71766301af31768
Andronomos <andronomos@proton.me>
2024-02-25 19:12:56 -0800

    Removed glass creative tab and added modern creative tab

--- src/main/java/andronomos/androbuildingblocks/registry/ModCreativeTabs.java
+++ src/main/java/andronomos/androbuildingblocks/registry/ModCreativeTabs.java
@@ -5,13 +5,14 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModCreativeTabs {
 	public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, AndroBuildingBlocks.MODID);
 
 	public static final String BASETABNAME = "androbuildingblocks_tab";
-	public static final String GLASSTABNAME = "androbuildingblocks_glass_tab";
+	public static final String MODERNTABNAME = "androbuildingblocks_modern_tab";
 
 	public static final RegistryObject<CreativeModeTab> ANDROBUILDINGBLOCKS_TAB = CREATIVE_MODE_TABS.register(BASETABNAME, () -> CreativeModeTab.builder()
 			.title(Component.translatable("creativetab." + BASETABNAME))
@@ -24,12 +25,14 @@ public class ModCreativeTabs {
 				output.accept(b);
 			})).build());
 
-	public static final RegistryObject<CreativeModeTab> ANDROBUILDINGBLOCKS_GLASS_TAB = CREATIVE_MODE_TABS.register(GLASSTABNAME, () -> CreativeModeTab.builder()
-			.title(Component.translatable("creativetab." + GLASSTABNAME))
-			.icon(BlockRegistry.WHITE_STRUCTURAL_GLASS.get().asItem()::getDefaultInstance)
+	public static final RegistryObject<CreativeModeTab> ANDROBUILDINGBLOCKS_MODERN_TAB = CREATIVE_MODE_TABS.register(MODERNTABNAME, () -> CreativeModeTab.builder()
+			.title(Component.translatable("creativetab." + MODERNTABNAME))
+			.icon(BlockRegistry.REINFORCED_CONCRETE_WHITE.get().asItem()::getDefaultInstance)
 			.displayItems((parameters, output) -> BlockRegistry.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(b -> {
-				switch (b.getClass().getSimpleName()) {
-					case "GlassBlock", "StainedGlassBlock", "StainedGlassPaneBlock" -> output.accept(b);
+				String blockName = ForgeRegistries.BLOCKS.getKey(b).getPath();
+
+				if(blockName.contains("concrete") || blockName.contains("structural_glass")) {
+					output.accept(b);
 				}
 			})).build());
 }
