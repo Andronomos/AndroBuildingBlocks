c071a42a9be4cfea01592000677e20b19b90ea46
Andronomos <andronomos@proton.me>
2024-03-03 18:44:50 -0800

    .

--- src/main/java/andronomos/androbuildingblocks/block/BlockVariants.java
+++
@@ -1,8 +0,0 @@
-package andronomos.androbuildingblocks.block;
-
-import java.util.Arrays;
-import java.util.List;
-
-public class BlockVariants {
-	public static final List<String> concreteColors = Arrays.asList("white", "blue", "black", "brown", "green", "gray", "purple", "red");
-}
