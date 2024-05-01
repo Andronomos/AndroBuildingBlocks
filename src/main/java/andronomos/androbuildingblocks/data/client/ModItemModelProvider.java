492e314830808c790721c6f8b8f55366ed6fba57
Andronomos <andronomos@proton.me>
2024-05-01 02:24:59 -0700

    Fixed missing steel ingot item model

--- src/main/java/andronomos/androbuildingblocks/data/client/ModItemModelProvider.java
+++ src/main/java/andronomos/androbuildingblocks/data/client/ModItemModelProvider.java
@@ -14,6 +14,7 @@ public class ModItemModelProvider extends ItemModelProvider {
 	@Override
 	protected void registerModels() {
 		createSingleTexture("sandpaper");
+		createSingleTexture("steel_ingot");
 	}
 
 	private ItemModelBuilder createSingleTexture(String name) {
