9568796c0c6f4a23a8b6ea038b4cb807c717a134
Andronomos <andronomos@proton.me>
2024-06-01 11:11:27 -0700

    Created catwalk railing loot table datgen

--- src/main/java/andronomos/androbuildingblocks/data/loot/ModBlockLootSubProvider.java
+++ src/main/java/andronomos/androbuildingblocks/data/loot/ModBlockLootSubProvider.java
@@ -1,9 +1,22 @@
 package andronomos.androbuildingblocks.data.loot;
 
+import andronomos.androbuildingblocks.block.CatwalkRailingBlock;
 import andronomos.androbuildingblocks.registry.BlockRegistry;
+import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,10 +32,44 @@ public class ModBlockLootSubProvider extends BlockLootSubProvider {
 		BlockRegistry.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(b -> {
 			switch (b.getClass().getSimpleName()) {
 				case "GlassBlock", "StainedGlassBlock", "StainedGlassPaneBlock" -> this.dropWhenSilkTouch(b);
+				case "CatwalkRailingBlock" -> this.railingBlock(b);
 				default -> this.dropSelf(b);
 			}
 		});
 	}
+
+	private void railingBlock(Block block) {
+		LootTable.Builder table = LootTable.lootTable();
+		LootPool.Builder pool = LootPool.lootPool();
+		pool.setRolls(ConstantValue.exactly(1));
+
+		LootItem.Builder<?> entry = LootItem.lootTableItem(block);
+
+		entry.apply(SetItemCountFunction.setCount(ConstantValue.exactly(1), true)
+				.when(LootItemBlockStatePropertyCondition.hasBlockStateProperties(block)
+						.setProperties(StatePropertiesPredicate.Builder.properties()
+								.hasProperty(BlockStateProperties.NORTH, true))));
+
+		entry.apply(SetItemCountFunction.setCount(ConstantValue.exactly(1), true)
+				.when(LootItemBlockStatePropertyCondition.hasBlockStateProperties(block)
+						.setProperties(StatePropertiesPredicate.Builder.properties()
+								.hasProperty(BlockStateProperties.NORTH, true))));
+
+		entry.apply(SetItemCountFunction.setCount(ConstantValue.exactly(1), true)
+				.when(LootItemBlockStatePropertyCondition.hasBlockStateProperties(block)
+						.setProperties(StatePropertiesPredicate.Builder.properties()
+								.hasProperty(BlockStateProperties.EAST, true))));
+
+		entry.apply(SetItemCountFunction.setCount(ConstantValue.exactly(1), true)
+				.when(LootItemBlockStatePropertyCondition.hasBlockStateProperties(block)
+						.setProperties(StatePropertiesPredicate.Builder.properties()
+								.hasProperty(BlockStateProperties.WEST, true))));
+
+		pool.add(entry);
+		table.withPool(pool);
+		this.add(block, table);
+	}
+
 
 	@Override
 	protected @NotNull Iterable<Block> getKnownBlocks() {
